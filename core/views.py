68d7103981e47eca3052bed22e23341251e4d13d
Zsolt Foldes <cicarichy@gmail.com>
2026-06-20 01:35:38 +0100

    feat: add home template and update view to render HTML > core/home.html

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Pet Sitter Home Page")
+    return render(request, "core/home.html")
