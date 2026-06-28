9a6856895ea3629b1c5f63574029757bd253e01d
Zsolt Foldes <cicarichy@gmail.com>
2026-06-29 00:49:27 +0100

    refactor: import User model for sitter_list view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Pet, Booking, Review
 from .forms import PetForm
 from .forms import BookingForm
