a88f0b3cde592ba83e54ba9ddadcfdf543f61b8e
Zsolt Foldes <cicarichy@gmail.com>
2026-07-06 02:20:15 +0100

    feat: add login-protected class-based availability list view in views.py

--- core/views.py
+++ core/views.py
@@ -1,11 +1,22 @@
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Pet, Booking
+from .models import Availability
 from .forms import SitterAvailability, SitterForm, SitterAvailabilityForm
 from .forms import PetForm, BookingForm, RegisterForm
+
+# Login-protected class-based view showing sitter availability list
+
+class AvailabilityListView(LoginRequiredMixin, ListView):
+    model = Availability
+    template_name = "availability/list.html"
+    context_object_name = "availability_list"
+    login_url = 'login'
 
 # Core placeholder views for each page in the site
 
