7907d309b6ae5baba256e147a496a9f8315628e9
Zsolt Foldes <cicarichy@gmail.com>
2026-09-07 00:24:10 +0100

    security: move SECRET_KEY to environment and update settings.py

--- pet_sitter/settings.py
+++ pet_sitter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-zxs*^mz-xt9(ezr^t88_95gcz7(&fpb0+5egkja-z4bz)9hbsl"
-)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
